ce95612f4bcf491b8cc802348d4b3e50a172dd8d
LastSkywalker <maxdr1998@gmail.com>
2026-03-12 11:42:53 +0300

    fix migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -12,6 +12,7 @@ import * as migration_20260311_210000_add_stats_and_section_fields from './20260
 import * as migration_20260311_220000_add_principles_section_tag from './20260311_220000_add_principles_section_tag'
 import * as migration_20260312_100000_add_site_settings_nav_fields from './20260312_100000_add_site_settings_nav_fields'
 import * as migration_20260312_110000_add_site_settings_common_fields from './20260312_110000_add_site_settings_common_fields'
+import * as migration_20260312_120000_add_locked_docs_knowledge_base_id from './20260312_120000_add_locked_docs_knowledge_base_id'
 
 export const migrations = [
   {
@@ -83,5 +84,10 @@ export const migrations = [
     up: migration_20260312_110000_add_site_settings_common_fields.up,
     down: migration_20260312_110000_add_site_settings_common_fields.down,
     name: '20260312_110000_add_site_settings_common_fields',
+  },
+  {
+    up: migration_20260312_120000_add_locked_docs_knowledge_base_id.up,
+    down: migration_20260312_120000_add_locked_docs_knowledge_base_id.down,
+    name: '20260312_120000_add_locked_docs_knowledge_base_id',
   },
 ];
